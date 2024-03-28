804c5b846302b7251abd6fba29c4e505328f812f
Tiny Tin <mr.tin.mihail.rybolovlev@gmail.com>
2024-03-28 20:43:42 +0300

    First version is done!

--- Assets/Scripts/Fireball/FireballCaster.cs
+++ Assets/Scripts/Fireball/FireballCaster.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FireballCaster : MonoBehaviour
@@ -7,17 +8,24 @@ public class FireballCaster : MonoBehaviour
     [SerializeField] Transform fireballSourceTransform;
     [SerializeField] private Transform uiInactiveBackground;
     [SerializeField] private TextMeshProUGUI uiCooldownTimerText;
+    [SerializeField] private List<AudioClip> popAudio;
     public float freezeTime;
     public float cooldown;
 
     private float _timer;
+    private AudioSource _playerAudioSource;
 
+    void Start()
+    {
+        _playerAudioSource = GetComponent<AudioSource>();
+    }
     void Update()
     {
         _timer += Time.deltaTime;
         if (Input.GetMouseButtonDown(1) && _timer >= cooldown)
         {
             var fireball = Instantiate(fireballPrefab, fireballSourceTransform.transform.position, fireballSourceTransform.transform.rotation);
+            _playerAudioSource.PlayOneShot(popAudio[Random.Range(0, popAudio.Count)]);
             fireball.timeForStopping = freezeTime;
             _timer = 0f;
         }
---
+++ Assets/Scripts/NextSceneOnEnter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneOnEnter : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
